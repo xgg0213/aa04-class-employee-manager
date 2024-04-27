82864183c1cb980ba81ba68f18bb1ac3d5817413
xgg0213 <m.eringan@gmail.com>
2024-04-27 10:25:32 -0700

    01-02

--- employee.js
+++ employee.js
@@ -1,3 +1,13 @@
 class Employee {
-    
+    constructor(name, salary, title, manager = null) {
+        this.name = name;
+        this.salary = salary;
+        this.title = title;
+        this.manager = manager;
+    }
 }
+
+module.exports = Employee;
+
+// const leo = new Employee('Leonardo', 90000, 'Ninja');
+// console.log(leo)
---
+++ manager.js
@@ -0,0 +1,29 @@
+const Employee = require('./employee');
+
+class Manager extends Employee {
+    constructor(name, salary, title, manager=null, employees=[]) {
+        super(name, salary, title, manager=null);
+        this.employees = employees;
+    }
+
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+}
+
+const splinter = new Manager('Splinter', 100000, 'Sensai');
+console.log('Before: ', splinter);
+
+const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
+const mikey = new Employee('Michelangelo', 90000, 'Ninja', splinter);
+const donnie = new Employee('Donatello', 90000, 'Ninja', splinter);
+const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
+
+splinter.addEmployee(leo);
+splinter.addEmployee(mikey);
+splinter.addEmployee(donnie);
+splinter.addEmployee(raph);
+
+console.log('After: ', splinter);
+
+module.exports = Manager;
