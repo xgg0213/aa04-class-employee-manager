8d93a441ed80710a5ae85e17a566865b81aa37d6
xgg0213 <m.eringan@gmail.com>
2024-04-27 13:11:34 -0700

    04

--- employee.js
+++ employee.js
@@ -8,9 +8,17 @@ class Employee {
             manager.addEmployee(this);
         };
     }
+
+    calculateBonus(multiplier) {
+        const bonus = (this.salary) * multiplier;
+        return bonus;
+    }
 }
 
 module.exports = Employee;
 
-// const leo = new Employee('Leonardo', 90000, 'Ninja', 'plinter');
-// console.log(leo)
+// const raph = new Employee('Raphael', 90000, 'Ninja');
+// const donny = new Employee('Donatello', 85000, 'Grasshopper');
+
+// console.log(raph.calculateBonus(0.25)); // => 22500
+// console.log(donny.calculateBonus(0.15)); // => 12750
--- manager.js
+++ manager.js
@@ -1,24 +1,51 @@
 const Employee = require('./employee');
 
 class Manager extends Employee {
-    constructor(name, salary, title, manager=null, employees=[]) {
-        super(name, salary, title, manager=null);
-        this.employees = employees;
+    constructor(name, salary, title, manager) { // manager cannot be set to null, because manager value needs to be updated everytime a Manager instance is invoked, not be foreced to null
+        super(name, salary, title, manager);
+        this.employees = [];
     }
 
     addEmployee(employee) {
         this.employees.push(employee);
     }
+
+    calculateBonus(multiplier) {
+        const bonus = (this.salary + this._totalSubSalary()) * multiplier;
+        // console.log(this._totalSubSalary());
+        return bonus;
+    }
+    _totalSubSalary() {
+        // console.log(this.employees);
+        let sum = 0;
+        for (const employee of this.employees) {
+            // console.log(employee);
+            if (employee instanceof Manager) {
+                sum += employee.salary + employee._totalSubSalary()
+            } else {
+                sum += employee.salary
+            }
+        }
+        return sum;
+    }
+
+    
+
+
 }
 
+
+
+
 const splinter = new Manager('Splinter', 100000, 'Sensei');
-console.log('Before: ', splinter);
+const leo = new Manager('Leonardo', 90000, 'Ninja', splinter);
+const raph = new Manager('Raphael', 90000, 'Ninja', leo);
+const mikey = new Employee('Michelangelo', 85000, 'Grasshopper', raph);
+const donnie = new Employee('Donatello', 85000, 'Grasshopper', raph);
 
-const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
-const mikey = new Employee('Michelangelo', 90000, 'Ninja', splinter);
-const donnie = new Employee('Donatello', 90000, 'Ninja', splinter);
-const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
-
-console.log('After: ', splinter);
+console.log(splinter.calculateBonus(0.05)); // => 22500 = 5000 + 4500 + 13000
+console.log(leo.calculateBonus(0.05)); // => 17500 = 4500 + 13000
+console.log(raph.calculateBonus(0.05)); // => 13000
+// console.log(leo);
 
 module.exports = Manager;
